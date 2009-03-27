a1870e9a3ca48f92d69744c11e71fae37e55613c
Jarkko Laine <jarkko@jlaine.net>
2009-03-27 10:50:28 +0200

    Added scaffolding for authors.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 ActionController::Routing::Routes.draw do |map|
-  map.resources :books
+  map.resources :books, :authors
 
   # The priority is based upon order of creation: first created -> highest priority.
 
