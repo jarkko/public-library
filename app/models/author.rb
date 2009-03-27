996ca877d1130608b8c564d3e2b2dd85a2da227c
Jarkko Laine <jarkko@jlaine.net>
2009-03-27 10:34:47 +0200

    Added validations to Author and Book models.

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,3 @@
 class Author < ActiveRecord::Base
+  validates_presence_of :first_name, :last_name
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,4 @@
 class Book < ActiveRecord::Base
+  validates_presence_of :isbn, :title
+  validates_uniqueness_of :isbn
 end
